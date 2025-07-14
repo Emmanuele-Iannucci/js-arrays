5b8404897d91c58d990ce591d5d1461dc56c34be
Emmanuele-Iannucci <emmanueleiannucci@gmail.com>
2025-07-14 19:26:27 +0200

    ex_3

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,15 +10,27 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = teachers [3];
-console.log (fourthTeacher); // Stampa: "Phil";
+
+const fourthTeacher = teachers[3];
+console.log(fourthTeacher); // Stampa: "Phil";
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-teachers [4] = "Patrick"; // Sostituisco "Carlo" con "Patrick"
-console.log (teachers); // risultato in console
+
+teachers[4] = "Patrick"; // Sostituisco "Carlo" con "Patrick"
+console.log(teachers); // risultato in console
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+
+let lastTeacher = null; // sostituisco const con let per poter assegnare dopo
+const newTeachers = []; // creo un array vuoto pronto per essere riempito
+for (let i = 0; i < teachers.length - 1; i++) {
+  newTeachers.push(teachers[i]); // copiamo tutti tranne l’ultimo
+}
+
+lastTeacher = teachers[teachers.length - 1];
+
+console.log(lastTeacher);    // L'ultimo insegnante ovvero 'Luca'
+console.log(newTeachers);    // Array senza l’ultimo elemento
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
